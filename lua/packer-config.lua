1827f5477e9659226de2d5ffff9cb13f86a5ba7c
luengor <aluengor0001@gmail.com>
2023-09-24 23:42:40 +0200

    moving around
    harpoon + tmux

--- lua/packer-config.lua
+++ lua/packer-config.lua
@@ -31,7 +31,28 @@ require('packer').startup(function(use)
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
+        },
+    }
+
+    -- harpoon
+    use {
+        "ThePrimeagen/harpoon",
+        requires = {
+            "nvim-lua/plenary.nvim"
         }
+    }
+
+    -- tmux moving
+    use { 
+        "alexghergh/nvim-tmux-navigation",
+        config = function()
+            -- Change the keymaps
+            local nvim_tmux_nav = require('nvim-tmux-navigation')
+            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        end
     }
 
 end)
