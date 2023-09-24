bc57a3f9f43411a6c8ae1a59bfead83a468941b9
luengor <aluengor0001@gmail.com>
2023-09-24 22:32:32 +0200

    start of v2

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,5 @@
 HOME = os.getenv('HOME')
 
-
 vim.cmd([[
 set nocompatible
 filetype plugin indent on
@@ -9,9 +8,7 @@ syntax on
 let mapleader=","
 
 set wildignore=*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx,*.o,*.gch
-let NvimTreeIgnore = ['\.o$', '\.gch$', '\.elf', '\.smdh']
 ]])
-
 
 vim.o.encoding = "utf-8"
 
@@ -19,7 +16,6 @@ vim.o.encoding = "utf-8"
 vim.o.number = true -- line number on the left
 vim.o.relativenumber = true
 vim.o.numberwidth = 3 -- always reserve 3 spaces for line number
-vim.o.signcolumn = 'yes' -- keep 1 column for coc.vim  check
 
 -- tabs thing 
 vim.o.autoindent = true
@@ -27,9 +23,6 @@ vim.o.smartindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
--- doxygen
-vim.g.load_doxygen_syntax = true
 
 -- no perl
 vim.g.loaded_perl_provider = false
@@ -40,9 +33,4 @@ vim.g.loaded_ruby_provider = false
 -- python3
 vim.g.python3_host_prog = '/home/tino/.config/nvim/vimvenv/bin/python3.11'
 vim.g.pyhton_highlight_space_errors = false
-
--- whatever this is (is for tree-nvim)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
 
