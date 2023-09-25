f27d95568578dfae0f64703cfd5a46a6d0488285
luengor <aluengor0001@gmail.com>
2023-09-25 19:13:44 +0200

    add pyright

--- lua/cmp-config.lua
+++ lua/cmp-config.lua
@@ -35,7 +35,7 @@ cmp.setup({
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
-local servers = { 'clangd' }
+local servers = { 'clangd', 'pyright' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         capabilities = capabilities,
