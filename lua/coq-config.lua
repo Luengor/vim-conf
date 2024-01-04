b3a05a1d194e08e736235dd007da4aaacc36e112
luengor <aluengor0001@gmail.com>
2024-01-04 18:07:37 +0100

    moved coq config

--- lua/coq-config.lua
+++ lua/coq-config.lua
@@ -1,15 +1,18 @@
 local lspconfig = require('lspconfig')
-local coq = require('coq')
 
--- Automatically start coq
+-- coq settings BEFORE require('coq') 
+-- (or auto_start won't work)
 vim.g.coq_settings = {
     auto_start = 'shut-up',
     ["keymap.jump_to_mark"] = "<c-y>",
     ["limits.completion_manual_timeout"] = 1,
 }
 
+-- coq
+local coq = require('coq')
+
 -- Servers
-local servers = { 'clangd', 'pyright', 'eslint', 'html', 'cssls' }
+local servers = { 'clangd', 'pyright', 'eslint', 'html', 'cssls', 'lua_ls' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup(coq.lsp_ensure_capabilities({}))
 end
